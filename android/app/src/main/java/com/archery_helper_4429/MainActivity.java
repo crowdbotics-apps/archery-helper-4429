223c91ba8cc12afaf1e5a4a25e5e6b1cb6714f25
Crowdbotics <team@crowdbotics.com>
2019-06-09 06:30:25 +0000

    initial commit

---
+++ android/app/src/main/java/com/archery_helper_4429/MainActivity.java
@@ -0,0 +1,15 @@
+package com.archery_helper_4429;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "archery_helper_4429";
+    }
+}
